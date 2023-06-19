0e40bf94c7fd60f3df1d4cd67815677f4e170711
Rohit <rohit87799@gmail.com>
2023-06-19 16:53:34 +0530

    CV updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CV from '../../assets/CV.pdf'
+import {FaFilePdf} from 'react-icons/fa'
+
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={CV} download className='btn'>Download CV <span><FaFilePdf/></span></a>
       <a href="#contact" className='btn btn-primary'> Let's Talk</a>
     </div>
   )
