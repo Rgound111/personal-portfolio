653cd5738d22b6601a8bdbe281f4650149e9b73b
Rohit <rohit87799@gmail.com>
2023-06-24 20:51:27 +0530

    Counterup

--- src/components/Counterup/Counterup.jsx
+++ src/components/Counterup/Counterup.jsx
@@ -20,13 +20,13 @@ const Counterup = () => {
                     <h1 className="h1">
                       + {counterStart && <CountUp start={0} end={1000} duration={3} delay={0} />}
                     </h1>
-                    < h3 className='h3'>Client Responses</h3>                   
+                    < h3 className='h3'>Employees</h3>                   
                 </div>
                 <div>
                     <h1 className="h1">
                         + {counterStart && <CountUp start={2000} end={6000} duration={3} delay={0} />}
                     </h1>
-                    < h3 className='h3'>Client Responses</h3>
+                    < h3 className='h3'>Total Employees</h3>
                 </div>
                 <div className='resp'>
                     <h1 className="h1">
